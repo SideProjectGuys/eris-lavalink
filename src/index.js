1da9f230985f96777cbd7ea57f68dd73442ec89e
Paradox <paradoxalcorp@gmail.com>
2018-11-01 16:48:53 +0100

    *attempt at making intellisense work*

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const Player = require('./Player');
 const PlayerManager = require('./PlayerManager');
 const Lavalink = require('./Lavalink');
 
+/** @typedef {Object} 
+ * @prop {import('./Player')} Player
+ * @prop {import('./PlayerManager')} PlayerManager
+ * @prop {import('./Lavalink')} Lavalink
+*/
 module.exports = {
 	Player,
 	PlayerManager,
